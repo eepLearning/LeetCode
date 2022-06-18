623726015672fefab4f39c4532a81bd4169a638f
JinHyuk Lim <eeplearning@unist.ac.kr>
2022-06-18 22:02:00 +0900

    Time: 231 ms (58.41%), Space: 15.6 MB (64.20%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -1,9 +1,10 @@
 #Topic: Two Pointers
+#난이도: 하
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
-        """
+        
         nums2 = list(nums)
         start = 0 
         end = len(nums2) -1 
@@ -19,3 +20,22 @@ class Solution:
             
         for i in range(len(nums)):
             nums[i] =nums2[i]
+        """
+        start = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i],nums[start] = nums[start],nums[i]
+                start += 1
+            
+            
+'''
+다른 풀이 
+추가 메모리 없이 해결(two points)
+
+start = 0
+for i in range(len(nums)):
+    if nums[i] != 0:
+        nums[i],nums[start] = nums[start],nums[i]
+        start += 1
+
+'''
