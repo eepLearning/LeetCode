1cdd93a83d61de259b2bac8dec3f2f82cc80c9cf
JinHyuk Lim <eeplearning@unist.ac.kr>
2022-06-18 23:26:17 +0900

    Time: 201 ms (43.93%), Space: 14.9 MB (40.86%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,6 +1,11 @@
 #Topic: Binary Search, Two Pointers
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        '''
+        hash 풀이법
+        시간 복잡도: n
+        공간복잡도: n
+        
         table = dict()
         for idx,n in enumerate(numbers):
     
@@ -8,7 +13,18 @@ class Solution:
                 table[(target-n)] = idx
             else:
                 return table[n]+1, idx+1
-
-                
+        216 ms / 15 MB
+        '''
+            
+        start = 0
+        end = len(numbers) - 1
         
-        
+        while start <= end:
+            s = numbers[start] + numbers[end]
+            if s == target:
+                return start + 1, end + 1
+            elif s < target:
+                start  += 1
+            else:
+                end -=1 
+            
