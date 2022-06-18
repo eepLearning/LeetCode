cb4d524cf779c6c199dc80303eefb8dccb97bd40
JinHyuk Lim <eeplearning@unist.ac.kr>
2022-06-18 23:51:15 +0900

    Time: 209 ms (88.44%), Space: 18.5 MB (46.72%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -4,5 +4,16 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+        '''
+        #그냥풀기
+        345 ms / 18.4 MB 
         s.reverse()
+        '''
+        start = 0
+        end = len(s) -1 
+        while start < end:
+            s[start],s[end] = s[end],s[start]
+            end -= 1
+            start += 1
+            
         
