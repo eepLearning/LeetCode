68c906c130d4544a483cfe024ba47c3c91d1c945
JinHyuk Lim <eeplearning@unist.ac.kr>
2022-06-19 21:43:36 +0900

    Time: 46 ms (75.61%), Space: 14.6 MB (82.43%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
@@ -2,7 +2,7 @@
 
 
 class Solution:
-    '''try1
+    '''try1 : 152ms/14.6mb (10%)
     def reverseWords(self, s: str) -> str:
         answer = []
         start = 0
@@ -18,6 +18,8 @@ class Solution:
             zero = zero + 1
         return ' '.join(answer)
         '''
+    #try2 : 62ms/14.6mb (50%)
+    #아씨 고민안하고 푸는게 훨씬 빠르네
     def reverseWords(self, s: str) -> str:
         return ' '.join([ss[::-1] for ss in s.split(" ")])
 
